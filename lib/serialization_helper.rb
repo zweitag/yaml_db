60993fe7a58d7cf2fe0bd7f7f3c85a291d4bf22a
Martin Honermeyer <martin.honermeyer@zweitag.de>
2019-02-11 22:38:16 +0100

    Skip foreign key deferring for Postgres when necessary method is missing
    This is needed for Rails < 4.2.0 compatibility.

--- lib/serialization_helper.rb
+++ lib/serialization_helper.rb
@@ -124,16 +124,18 @@ module SerializationHelper
     def self.defer_fk_constraints(&block)
       case database_type
       when :postgresql
-        # make all fk constraints deferrable
-        tables.each do |table|
-          fk_constraints_on_table = ActiveRecord::Base.connection.foreign_keys(table)
-          fk_constraints_on_table.each do |fk_constraint|
-            quoted_table_name = SerializationHelper::Utils.quote_table(table)
-            ActiveRecord::Base.connection.execute("ALTER TABLE #{quoted_table_name} ALTER CONSTRAINT #{fk_constraint.name} DEFERRABLE INITIALLY IMMEDIATE")
+        if ActiveRecord::Base.connection.respond_to?(:foreign_keys)     # Rails <4.2 compatibility
+          # make all fk constraints deferrable
+          tables.each do |table|
+            fk_constraints_on_table = ActiveRecord::Base.connection.foreign_keys(table)
+            fk_constraints_on_table.each do |fk_constraint|
+              quoted_table_name = SerializationHelper::Utils.quote_table(table)
+              ActiveRecord::Base.connection.execute("ALTER TABLE #{quoted_table_name} ALTER CONSTRAINT #{fk_constraint.name} DEFERRABLE INITIALLY IMMEDIATE")
+            end
           end
+          # defer all fk constraints
+          ActiveRecord::Base.connection.execute("SET CONSTRAINTS ALL DEFERRED")
         end
-        # defer all fk constraints
-        ActiveRecord::Base.connection.execute("SET CONSTRAINTS ALL DEFERRED")
         yield block
       when :mysql
         ActiveRecord::Base.connection.execute("SET foreign_key_checks = 0")
